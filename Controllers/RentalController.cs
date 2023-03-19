0e57e352d7b83e82a370725ce7d8c5036526537e
Krzysztof Grzywaczyk <krzysztof.grzywaczyk@gmail.com>
2023-03-19 23:15:45 +0100

    Implement Models and Controller

---
+++ Controllers/RentalController.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using CarRentalAPI.Entities;
+using CarRentalAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace CarRentalAPI.Controllers
+{
+    [Route("api/rentaloffices")]
+    public class RentalController : ControllerBase
+    {
+        private readonly RentalDbContext _dbContext;
+
+        private readonly IMapper _mapper;
+
+        public RentalController(RentalDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RentalOfficeDto>> GetAll()
+        {
+            var rentals = _dbContext.rentalOffices
+                .Include(r => r.Address)
+                .Include(r => r.Cars)
+                .ToList();
+
+            var rentalsDto = _mapper.Map<List<RentalOfficeDto>>(rentals);
+
+            return Ok(rentalsDto);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<RentalOfficeDto> Get([FromRoute] int id) 
+        {
+            var rental = _dbContext.rentalOffices
+                .Include(r => r.Address)
+                .Include(r => r.Cars)
+                .FirstOrDefault(r => r.Id == id);
+
+            
+
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var rentalDto = _mapper.Map<RentalOfficeDto>(rental);
+                return Ok(rentalDto);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateRentalOffice([FromBody] CreateRentalOfficeDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var rentalOffice = _mapper.Map<RentalOffice>(dto);
+            _dbContext.rentalOffices.Add(rentalOffice);
+            _dbContext.SaveChanges();
+
+            return Created($"/api/rentaloffices/{rentalOffice.Id}", null);
+        }
+      
+    }
+}
---
+++ Models/CreateRentalOfficeDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalAPI.Models
+{
+    public class CreateRentalOfficeDto
+    {
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool AcceptUnder23 { get; set; }
+        public string ConntactEmail { get; set; }
+        public string ConntactNumber { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string City { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+
+    }
+}
