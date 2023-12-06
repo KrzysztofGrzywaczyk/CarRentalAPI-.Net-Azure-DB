5048e0943904029363ef5cd411d7615b7870963f
KrzysztofGrzywaczyk <33352560+KrzysztofGrzywaczyk@users.noreply.github.com>
2023-12-06 13:32:44 +0100

    Revert "Develop futures"

---
+++ Entities/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRentalAPI.Entities
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public virtual RentalOffice RentalOffice { get; set; }
+
+
+
+        
+    }
+}
---
+++ RentalSeeder.cs
@@ -0,0 +1,71 @@
+﻿using CarRentalAPI.Entities;
+
+namespace CarRentalAPI
+{
+    public class RentalSeeder
+    {
+        private readonly RentalDbContext _dbContext;
+        public RentalSeeder(RentalDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Seed()
+        {
+            if (_dbContext.Database.CanConnect())
+            {
+                if (!_dbContext.rentalOffices.Any())
+                {
+                    var rentals = GetRentalOffices();
+                    _dbContext.rentalOffices.AddRange(rentals);
+                    _dbContext.SaveChanges();
+                }
+            }
+        }
+
+        private IEnumerable<RentalOffice> GetRentalOffices()
+        {
+            var rentals = new List<RentalOffice>()
+            {
+                new RentalOffice()
+                {
+                    Name = "Example One Car Rental",
+                    Description = "It is short description of what kind of Car Rental Office is it",
+                    Category = "Luxury cars",
+                    AcceptUnder23 = false,
+                    ConntactEmail = "luxury@example.com",
+                    ConntactNumber = "0123456789",
+                    Address = new Address()
+                    {
+                        City = "Wroclaw",
+                        Street = "Example Street 10",
+                        PostalCode = "50-419"
+                    },
+                    Cars = new List<Car>()
+                    {
+                        new Car()
+                        {
+                            PlateNumber = "DW 3HL50",
+                            Brand = "Mercedes",
+                            Model = "S 600 Long",
+                            Year = 2021,
+                            Segment = 'F'
+                        },
+
+                        new Car()
+                        {
+                            PlateNumber = "DX 12E43",
+                            Brand = "Cadillac",
+                            Model = "XTS",
+                            Year = 2020,
+                            Segment = 'F'
+                        },
+                    },
+                }
+            };
+
+            return rentals;
+        }
+
+    }
+
+}
