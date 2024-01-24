bb996749cddad05aff894bd39f4e9a0a420309d6
Krzysztof Grzywaczyk <krzysztof.grzywaczyk@mesh-systems.com>
2024-01-24 14:58:37 +0100

    Change folders structure

---
+++ src/Controllers/RolesController.cs
@@ -0,0 +1,33 @@
+﻿using CarRentalAPI.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using CarRentalAPI.Entities;
+
+namespace CarRentalAPI.Controllers;
+
+//to delete
+
+[ApiController]
+[Route("roles")]
+public class RoleController : ControllerBase
+{
+    private readonly ILogHandler _logHandler;
+    private readonly RentalDbContext _dbContext;
+    private const string entityType = "Car";
+
+    public RoleController(ILogHandler logHandler, RentalDbContext dbContext)
+    {
+        _logHandler = logHandler;
+        _dbContext = dbContext;
+    }
+    [HttpGet]
+    public ActionResult GetAllRoles()
+    {
+        _logHandler.LogNewRequest(entityType, ILogHandler.RequestEnum.GET);
+
+         var roles = _dbContext.roles
+                .ToList();
+
+        return Ok(roles);
+       
+    }
+}
---
+++ tests/CarRentalAPI.UnitTests/Services/CarServiceTests.cs
@@ -0,0 +1,16 @@
+namespace CarRentalAPI.UnitTests.Services
+{
+    public class CarServiceTests
+    {
+        [Fact]
+        public void CreateCar_ForExistingCarInExistingRentallOffice_ShouldAddNewCarToDatabase()
+        {
+        }
+
+        [Fact]
+        public void DeleteCar_ForExistingCarId_ShouldDeleteCarFromDatabase()
+        {
+        }
+
+    }
+}
